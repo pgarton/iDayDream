97a85a1df2df701013f293dd3e02717241609e55
pgarton <55724503+pgarton@users.noreply.github.com>
2019-11-06 17:03:24 -0800

    Add files via upload
    truncate_test_data
    truncate table volunteer_roles;
    truncate table volunteer_references;
    truncate table volunteers;
    truncate table youth;

---
+++ database/test_data/truncate_test_data.sql
@@ -0,0 +1,25 @@
+use paulsbun_idaydream;
+-- use idaydream;
+
+
+SET FOREIGN_KEY_CHECKS = 0; 
+truncate table volunteer_roles;
+truncate table volunteer_references;
+truncate table volunteers;
+truncate table youth;
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
