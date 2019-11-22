08e244073ff073ec4e67e564f898d91ba755831c
pgarton <55724503+pgarton@users.noreply.github.com>
2019-11-22 10:31:42 -0800

    Update paulsbun_idaydream_schema.sql
    generic schema changes

--- database/ddl/paulsbun_idaydream_schema.sql
+++ database/ddl/paulsbun_idaydream_schema.sql
@@ -12,7 +12,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- drop schema idaydream;
 -- create schema idaydream;
 -- -----------------------------------------------------
-USE pgartong_idaydream ;
 
 SET FOREIGN_KEY_CHECKS = 0;
 
